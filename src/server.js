e1c5491928fb7374d66565efc0b97c6c8649257b
ViniciusSBezerra <viniciussouza2599@gmail.com>
2020-08-01 09:04:34 -0300

    Ultimo dia da NLW, Projeto Finalizado!

--- src/server.js
+++ src/server.js
@@ -2,7 +2,12 @@ const express = require("express")
 
 const server = express()
 
+//pegar DB
+
+const db = require ("./database/db")
+
 const nunJuscks = require("nunjucks")
+const { response } = require("express")
 nunJuscks.configure("src/views", {
    express: server,
    noCache: true
@@ -11,6 +16,12 @@ nunJuscks.configure("src/views", {
 
 //configurar uma pasta publica
 server.use(express.static("public"))
+
+//habilitar o uso do requiry.body
+server.use(express.urlencoded({
+   extended:true
+}))
+
 //criando um servidor
 server.get("/", (require, response) => {
   return response.render("index.html", {title: "Seu marketplace de coleta de resíduos"})
@@ -20,8 +31,67 @@ server.get("/create-point", (require, response) => {
    return response.render("create-point.html")
 })
 
+server.post("/savepoint", (require, response) =>{
+//inserir dados na tabela
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+
+        )VALUES(?,?,?,?,?,?,?);  
+    `
+    
+    const values = [
+      require.body.image,
+      require.body.name,
+      require.body.address,
+      require.body.address2,
+      require.body.state,
+      require.body.city,
+      require.body.items
+        
+    ]
+
+    function afterInsertData(err){
+        if(err) {
+            return console.log(err)
+        }
+
+        return response.render("create-point.html", {saved:true})
+        
+    }
+    db.run(query, values, afterInsertData)
+   
+    
+     
+})
+
 server.get("/search", (require, response) => {
-   return response.render("search-results.html")
+
+   const search = require.query.search
+
+   if(search == ""){
+      return response.render("search-results.html", { total: 0 })
+   }
+   //pegar dados do banco de dados
+   db.all(`SELECT * FROM places WHERE city LIKE  '%${search}%'`, function(err, rows){
+      if(err) {
+         return console.log(err)
+      }   
+      
+      const total = rows.length
+      //mostrar a pagina html com os dados do bd
+      return response.render("search-results.html", { places: rows , total: total })
+
+
+   })
+
+   
 })
 
 
